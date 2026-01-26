a9af5a02338b133605f179ab9dc11fe946081ca8
Trimi123123 <trimmerovci10@gmail.com>
2026-01-26 19:16:17 +0100

    2

---
+++ module 2/main.py
@@ -0,0 +1,11 @@
+man = ("hey", "hello", "hi")
+print(man(0))
+
+smth = ()
+print(smth)
+
+smth2 = ("Trim", 13, "programmer")
+
+name , age , job =smth2
+
+print("he is", name ,"he is", age ,"he works as a", job )
