81742e2204f6050f82442ac541bc36f0c4309ae3
Trimi123123 <trimmerovci10@gmail.com>
2026-02-04 19:11:33 +0100

    Create main1.py

---
+++ main1.py
@@ -0,0 +1,27 @@
+
+
+names = ["Dreni", "Gerti", "Deon", "Trimi"]
+
+for name in names:
+    print(name)
+
+letters = "hello,world"
+
+for character in letters:
+    if not character.isalpha():
+        print(character)
+        break
+
+# for number in range(1,6):
+#     print(number)
+
+numbers=[1,5,7,42,59,86,75,12]
+maximum = max(numbers)
+print(maximum)
+
+
+
+
+
+
+
